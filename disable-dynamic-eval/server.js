1fc93a2311a34a1ea4257543a6c99d9e3c195def
Liran Tal <liran.tal@gmail.com>
2025-01-20 20:00:48 +0000

    feat: add example of dynamic code still running

--- disable-dynamic-eval/server.js
+++ disable-dynamic-eval/server.js
@@ -1,8 +1,10 @@
-const http = require("http");
-const url = require("url");
+const vm = require('node:vm');
+const http = require("node:http");
+const url = require("node:url");
 
 const hostname = "127.0.0.1";
 const port = 3000;
+
 
 const server = http.createServer((req, res) => {
   const queryObject = url.parse(req.url, true).query;
@@ -17,6 +19,10 @@ const server = http.createServer((req, res) => {
       result = "Error evaluating expression.";
     }
   }
+
+  // This however, still works!
+  const code = 'console.log("Hello, World!")';
+  vm.runInThisContext(code);
 
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
